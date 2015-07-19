5a925d8b5cf9236c67466d81a9ed657117460dd7
pfefferle <pfefferle@gmail.com>
2015-07-20 00:32:25 +0200

    small rewrite to check theme support
    see #12

--- genesis.php
+++ genesis.php
@@ -1,39 +1,39 @@
 <?php
 /* The Genesis Framework (http://studiopress.com/themes/genesis/) has
  * additional hooks that allow adding microformat classes without needing to
- * add wrapper divs.  This file adjusts the uf2 plugin to take advantage of
+ * add wrapper divs.	This file adjusts the uf2 plugin to take advantage of
  * those hooks when Genesis is present.
  */
 
 // replace some post and comment hooks
-remove_filter( 'the_title', 'uf2_the_title', 99, 1 );
-remove_filter( 'the_content', 'uf2_the_post', 99, 1 );
-remove_filter( 'the_excerpt', 'uf2_the_excerpt', 99, 1 );
-remove_filter( 'the_author', 'uf2_the_author', 99, 1 );
-remove_filter( 'comment_text', 'uf2_comment_text', 99, 1 );
-remove_filter( 'get_comment_author_link', 'uf2_author_link' );
-remove_filter( 'comment_class', 'uf2_comment_classes' );
+remove_filter( 'the_title', array( 'Uf2Plugin', 'the_title' ), 99, 1 );
+remove_filter( 'the_content', array( 'Uf2Plugin', 'the_post' ), 99, 1 );
+remove_filter( 'the_excerpt', array( 'Uf2Plugin', 'the_excerpt' ), 99, 1 );
+remove_filter( 'the_author', array( 'Uf2Plugin', 'the_author' ), 99, 1 );
+remove_filter( 'comment_text', array( 'Uf2Plugin', 'comment_text' ), 99, 1 );
+remove_filter( 'get_comment_author_link', array( 'Uf2Plugin', 'author_link' ) );
+remove_filter( 'comment_class', array( 'Uf2Plugin', 'comment_classes' ) );
 
 
 /**
  * Markup site title as p-name on archive pages.
  */
-function uf2_genesis_attr_site_title($attr) {
-  if (!is_singular()) {
-    $attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-name';
-  }
-  return $attr;
+function uf2_genesis_attr_site_title( $attr ) {
+	if ( ! is_singular() ) {
+		$attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-name';
+	}
+	return $attr;
 }
 add_filter( 'genesis_attr_site-title', 'uf2_genesis_attr_site_title' );
 
 /**
- * Add entry permalink with microformats 2 support.  This is a little ugly
+ * Add entry permalink with microformats 2 support.	This is a little ugly
  * because we're adding a non-visible anchor tag with no text, but
  * unfortunately is necessary since Genesis doesn't consistently include a
  * permalink on individual posts.
  */
 function uf2_genesis_entry_permalink() {
-  echo '<a class="u-url" href="' . get_permalink() .'"></a>';
+	echo '<a class="u-url" href="' . get_permalink() .'"></a>';
 }
 add_filter( 'genesis_entry_header', 'uf2_genesis_entry_permalink' );
 
@@ -41,8 +41,8 @@ add_filter( 'genesis_entry_header', 'uf2_genesis_entry_permalink' );
  * Adds microformats v2 support to the post title.
  */
 function uf2_genesis_attr_entry_title( $attr ) {
-  $attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-name';
-  return $attr;
+	$attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-name';
+	return $attr;
 }
 add_filter( 'genesis_attr_entry-title', 'uf2_genesis_attr_entry_title', 20 );
 
@@ -50,8 +50,8 @@ add_filter( 'genesis_attr_entry-title', 'uf2_genesis_attr_entry_title', 20 );
  * Adds microformats v2 support to the post content.
  */
 function uf2_genesis_attr_entry_content( $attr ) {
-  $attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'e-content';
-  return $attr;
+	$attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'e-content';
+	return $attr;
 }
 add_filter( 'genesis_attr_entry-content', 'uf2_genesis_attr_entry_content', 20 );
 
@@ -59,8 +59,8 @@ add_filter( 'genesis_attr_entry-content', 'uf2_genesis_attr_entry_content', 20 )
  * Adds microformats v2 support to the post date.
  */
 function uf2_genesis_attr_entry_time( $attr ) {
-  $attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'dt-published';
-  return $attr;
+	$attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'dt-published';
+	return $attr;
 }
 add_filter( 'genesis_attr_entry-time', 'uf2_genesis_attr_entry_time', 20 );
 
@@ -68,8 +68,8 @@ add_filter( 'genesis_attr_entry-time', 'uf2_genesis_attr_entry_time', 20 );
  * Adds microformats v2 support to the post author.
  */
 function uf2_genesis_attr_entry_author( $attr ) {
-  $attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-author h-card';
-  return $attr;
+	$attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-author h-card';
+	return $attr;
 }
 add_filter( 'genesis_attr_entry-author', 'uf2_genesis_attr_entry_author', 20 );
 
@@ -77,8 +77,8 @@ add_filter( 'genesis_attr_entry-author', 'uf2_genesis_attr_entry_author', 20 );
  * Adds microformats v2 support to the post author link.
  */
 function uf2_genesis_attr_entry_author_link( $attr ) {
-  $attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'u-url';
-  return $attr;
+	$attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'u-url';
+	return $attr;
 }
 add_filter( 'genesis_attr_entry-author-link', 'uf2_genesis_attr_entry_author_link', 20 );
 
@@ -86,8 +86,8 @@ add_filter( 'genesis_attr_entry-author-link', 'uf2_genesis_attr_entry_author_lin
  * Adds microformats v2 support to the post author name.
  */
 function uf2_genesis_attr_entry_author_name( $attr ) {
-  $attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-name';
-  return $attr;
+	$attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-name';
+	return $attr;
 }
 add_filter( 'genesis_attr_entry-author-name', 'uf2_genesis_attr_entry_author_name', 20 );
 
@@ -95,8 +95,8 @@ add_filter( 'genesis_attr_entry-author-name', 'uf2_genesis_attr_entry_author_nam
  * Adds microformat 2 classes to the array of comment classes.
  */
 function uf2_genesis_attr_comment( $attr ) {
-  $attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-comment h-entry';
-  return $attr;
+	$attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-comment h-entry';
+	return $attr;
 }
 add_filter( 'genesis_attr_comment', 'uf2_genesis_attr_comment', 20 );
 
@@ -104,8 +104,7 @@ add_filter( 'genesis_attr_comment', 'uf2_genesis_attr_comment', 20 );
  * Adds microformats v2 support to the comment author.
  */
 function uf2_genesis_attr_comment_author( $attr ) {
-  $attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-author h-card';
-  return $attr;
+	$attr['class'] .= ( $attr['class'] ? ' ' : '' ) . 'p-author h-card';
+	return $attr;
 }
 add_filter( 'genesis_attr_comment-author', 'uf2_genesis_attr_comment_author', 20 );
-
--- uf2.php
+++ uf2.php
@@ -8,166 +8,183 @@
  Version: 1.0.0-dev
 */
 
-add_action( 'init', 'uf2_genesis_init' );
-function uf2_genesis_init() {
-  if ( function_exists( 'genesis_html5' ) && genesis_html5() ) {
-    include_once dirname(__FILE__) . '/genesis.php';
-  }
-}
+add_action( 'init', array( 'Uf2Plugin', 'init' ) );
 
 /**
- * Adds custom classes to the array of post classes.
+ * Adds microformats2 support to your WordPress theme
+ *
+ * @author Matthias Pfefferle
  */
-function uf2_post_classes( $classes ) {
-  if (!is_singular()) {
-    return uf2_post_classes_helper($classes);
-  } else {
-    $classes = array_diff($classes, array('hentry'));
-  }
+class Uf2Plugin {
+	/**
+	 * Initialize plugin
+	 */
+	public static function init() {
+		// check if theme already supports Microformats2
+		if ( current_theme_supports( 'microformats2' ) ) {
+			return;
+		}
 
-  return $classes;
+		add_filter( 'post_class', array( 'Uf2Plugin', 'post_classes' ) );
+		add_filter( 'body_class', array( 'Uf2Plugin', 'body_classes' ) );
+		add_filter( 'comment_class', array( 'Uf2Plugin', 'comment_classes' ) );
+		add_filter( 'get_comment_author_link', array( 'Uf2Plugin', 'author_link' ) );
+		add_filter( 'get_avatar', array( 'Uf2Plugin', 'get_avatar' ) );
+		add_filter( 'the_title', array( 'Uf2Plugin', 'the_title' ), 99, 1 );
+		add_filter( 'the_content', array( 'Uf2Plugin', 'the_post' ), 99, 1 );
+		add_filter( 'comment_text', array( 'Uf2Plugin', 'comment_text' ), 99, 1 );
+		add_filter( 'the_excerpt', array( 'Uf2Plugin', 'the_excerpt' ), 99, 1 );
+		add_filter( 'the_author', array( 'Uf2Plugin', 'the_author' ), 99, 1 );
+
+		if ( function_exists( 'genesis_html5' ) && genesis_html5() ) {
+			include_once dirname( __FILE__ ) . '/genesis.php';
+		}
+	}
+
+	/**
+	 * Adds custom classes to the array of post classes.
+	 */
+	public static function post_classes( $classes ) {
+		if ( ! is_singular() ) {
+			return self::post_classes_helper( $classes );
+		} else {
+			$classes = array_diff( $classes, array( 'hentry' ) );
+		}
+
+		return $classes;
+	}
+
+	/**
+	 * Adds custom classes to the array of body classes.
+	 */
+	public static function body_classes( $classes ) {
+		if ( ! is_singular() ) {
+			$classes[] = 'h-feed';
+		} else {
+			$classes = post_classes_helper( $classes );
+		}
+
+		return $classes;
+	}
+
+	/**
+	 * Adds custom classes to the array of comment classes.
+	 */
+	public static function comment_classes( $classes ) {
+		$classes[] = 'p-comment';
+		$classes[] = 'h-entry';
+
+		return $classes;
+	}
+
+	/**
+	 * Adds microformats v2 support to the comment_author_link.
+	 */
+	public static function author_link( $link ) {
+		// Adds a class for microformats v2
+		return preg_replace( '/(class\s*=\s*[\"|\'])/i', '${1}u-url ', $link );
+	}
+
+	/**
+	 * Adds microformats v2 support to the get_avatar() method.
+	 */
+	public static function get_avatar( $tag ) {
+		// Adds a class for microformats v2
+		return preg_replace( '/(class\s*=\s*[\"|\'])/i', '${1}u-photo ', $tag );
+	}
+
+	/**
+	 * Adds microformats v2 support to the post title.
+	 */
+	public static function the_title( $title ) {
+		if ( ! is_admin() && in_the_loop() ) {
+			return "<span class='p-name'>$title</span>";
+		}
+
+		return $title;
+	}
+
+	/**
+	 * Adds microformats v2 support to the post.
+	 */
+	public static function the_post( $post ) {
+		if ( ! is_admin() ) {
+			return "<div class='e-content'>$post</div>";
+		}
+
+		return $post;
+	}
+
+	/**
+	 * Adds microformats v2 support to the comment.
+	 */
+	public static function comment_text( $comment ) {
+		if ( ! is_admin() ) {
+			return "<div class='e-content p-name p-summary'>$comment</div>";
+		}
+
+		return $comment;
+	}
+
+	/**
+	 * Adds microformats v2 support to the excerpt.
+	 */
+	public static function the_excerpt( $post ) {
+		if ( ! is_admin() ) {
+			return "<div class='e-content p-summary'>$post</div>";
+		}
+
+		return $post;
+	}
+
+	/**
+	 * Adds microformats v2 support to the author.
+	 */
+	public static function the_author( $author ) {
+		if ( ! is_admin() ) {
+			return "<span class='p-author h-card'>$author</span>";
+		}
+
+		return $author;
+	}
+
+	public static function post_classes_helper( $classes ) {
+		// Adds a class for microformats v2
+		$classes = array_diff( $classes, array( 'hentry' ) );
+		$classes[] = 'h-entry';
+		$classes[] = 'hentry';
+
+		// adds microformats 2 activity-stream support
+		// for pages and articles
+		if ( 'page' == get_post_type() ) {
+			$classes[] = 'h-as-page';
+		}
+		if ( ! get_post_format() && 'post' == get_post_type() ) {
+			$classes[] = 'h-as-article';
+		}
+
+		// adds some more microformats 2 classes based on the
+		// posts "format"
+		switch ( get_post_format() ) {
+			case 'aside':
+			case 'status':
+				$classes[] = 'h-as-note';
+				break;
+			case 'audio':
+				$classes[] = 'h-as-audio';
+				break;
+			case 'video':
+				$classes[] = 'h-as-video';
+				break;
+			case 'gallery':
+			case 'image':
+				$classes[] = 'h-as-image';
+				break;
+			case 'link':
+				$classes[] = 'h-as-bookmark';
+				break;
+		}
+
+		return $classes;
+	}
 }
-add_filter( 'post_class', 'uf2_post_classes' );
-
-/**
- * Adds custom classes to the array of body classes.
- */
-function uf2_body_classes( $classes ) {
-  if (!is_singular()) {
-    $classes[] = "h-feed";
-  } else {
-    $classes = uf2_post_classes_helper($classes);
-  }
-
-  return $classes;
-}
-add_filter( 'body_class', 'uf2_body_classes' );
-
-/**
- * Adds custom classes to the array of comment classes.
- */
-function uf2_comment_classes( $classes ) {
-  $classes[] = "p-comment";
-  $classes[] = "h-entry";
-
-  return $classes;
-}
-add_filter( 'comment_class', 'uf2_comment_classes' );
-
-function uf2_post_classes_helper($classes) {
-  // Adds a class for microformats v2
-  $classes = array_diff($classes, array('hentry'));
-  $classes[] = 'h-entry';
-  $classes[] = 'hentry';
-
-  // adds microformats 2 activity-stream support
-  // for pages and articles
-  if ( get_post_type() == "page" ) {
-    $classes[] = "h-as-page";
-  }
-  if ( !get_post_format() && get_post_type() == "post" ) {
-    $classes[] = "h-as-article";
-  }
-
-  // adds some more microformats 2 classes based on the
-  // posts "format"
-  switch ( get_post_format() ) {
-    case "aside":
-    case "status":
-      $classes[] = "h-as-note";
-      break;
-    case "audio":
-      $classes[] = "h-as-audio";
-      break;
-    case "video":
-      $classes[] = "h-as-video";
-      break;
-    case "gallery":
-    case "image":
-      $classes[] = "h-as-image";
-      break;
-    case "link":
-      $classes[] = "h-as-bookmark";
-      break;
-  }
-
-  return $classes;
-}
-
-/**
- * Adds microformats v2 support to the comment_author_link.
- */
-function uf2_author_link( $link ) {
-  // Adds a class for microformats v2
-  return preg_replace('/(class\s*=\s*[\"|\'])/i', '${1}u-url ', $link);
-}
-add_filter( 'get_comment_author_link', 'uf2_author_link' );
-
-/**
- * Adds microformats v2 support to the get_avatar() method.
- */
-function uf2_get_avatar( $tag ) {
-  // Adds a class for microformats v2
-  return preg_replace('/(class\s*=\s*[\"|\'])/i', '${1}u-photo ', $tag);
-}
-add_filter( 'get_avatar', 'uf2_get_avatar' );
-
-/**
- * Adds microformats v2 support to the post title.
- */
-function uf2_the_title( $title ) {
-  if (!is_admin() && in_the_loop()) {
-    return "<span class='p-name'>$title</span>";
-  }
-
-  return $title;
-}
-add_filter( 'the_title', 'uf2_the_title', 99, 1 );
-
-/**
- * Adds microformats v2 support to the post.
- */
-function uf2_the_post( $post ) {
-  if (!is_admin()) {
-    return "<div class='e-content'>$post</div>";
-  }
-
-  return $post;
-}
-add_filter( 'the_content', 'uf2_the_post', 99, 1 );
-
-/**
- * Adds microformats v2 support to the comment.
- */
-function uf2_comment_text( $comment ) {
-  if (!is_admin()) {
-    return "<div class='e-content p-name p-summary'>$comment</div>";
-  }
-
-  return $comment;
-}
-add_filter( 'comment_text', 'uf2_comment_text', 99, 1 );
-
-/**
- * Adds microformats v2 support to the excerpt.
- */
-function uf2_the_excerpt( $post ) {
-  if (!is_admin()) {
-    return "<div class='e-content p-summary'>$post</div>";
-  }
-
-  return $post;
-}
-add_filter( 'the_excerpt', 'uf2_the_excerpt', 99, 1 );
-
-/**
- * Adds microformats v2 support to the author.
- */
-function uf2_the_author( $author ) {
-  if (!is_admin()) {
-    return "<span class='p-author h-card'>$author</span>";
-  }
-
-  return $author;
-}
-add_filter( 'the_author', 'uf2_the_author', 99, 1 );

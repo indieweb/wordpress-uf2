09d2d95ede673550706ff2458603231b15d502f9
David Shanske <david@shanske.com>
2017-06-17 14:06:43 -0500

    Remove mf2 AS support due fact it never caught on

--- uf2.php
+++ uf2.php
@@ -154,37 +154,6 @@ class Uf2Plugin {
 		$classes[] = 'h-entry';
 		$classes[] = 'hentry';
 
-		// adds microformats 2 activity-stream support
-		// for pages and articles
-		if ( 'page' == get_post_type() ) {
-			$classes[] = 'h-as-page';
-		}
-		if ( ! get_post_format() && 'post' == get_post_type() ) {
-			$classes[] = 'h-as-article';
-		}
-
-		// adds some more microformats 2 classes based on the
-		// posts "format"
-		switch ( get_post_format() ) {
-			case 'aside':
-			case 'status':
-				$classes[] = 'h-as-note';
-				break;
-			case 'audio':
-				$classes[] = 'h-as-audio';
-				break;
-			case 'video':
-				$classes[] = 'h-as-video';
-				break;
-			case 'gallery':
-			case 'image':
-				$classes[] = 'h-as-image';
-				break;
-			case 'link':
-				$classes[] = 'h-as-bookmark';
-				break;
-		}
-
 		return $classes;
 	}
 }

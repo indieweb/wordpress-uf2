576ef3c73b76156bbfe26c78fb2f42b4f110e089
David Shanske <david@shanske.com>
2017-06-17 17:11:27 -0500

    Update Avatar Function to use new data filter

--- includes/author.php
+++ includes/author.php
@@ -9,7 +9,7 @@ class UF2_Author {
 	 * Initialize plugin
 	 */
 	public function __construct() {
-		add_filter( 'get_avatar', array( $this, 'get_avatar' ) );
+		add_filter( 'get_avatar_data', array( $this, 'get_avatar_data' ), 10, 2 );
 		add_filter( 'the_author', array( $this, 'the_author' ), 99, 1 );
 	}
 
@@ -22,11 +22,22 @@ class UF2_Author {
 	}
 
 	/**
-	 * Adds microformats v2 support to the get_avatar() method.
+	 * Adds microformats v2 support to the get_avatar_data() method.
 	 */
-	public static function get_avatar( $tag ) {
+	public static function get_avatar_data( $id_or_email, $args ) {
 		// Adds a class for microformats v2
-		return preg_replace( '/(class\s*=\s*[\"|\'])/i', '${1}u-photo ', $tag );
+		if ( ! isset( $args['class'] ) ) {
+			$args['class'] = array();
+		}
+
+		if ( is_string( $args['class'] ) ) {
+			$args['class'] = explode( ' ', $args['class'] );
+			$args['class'][] = 'u-photo';
+		}
+		else { 
+			$args['class'][] = 'u-photo';
+		}
+		return $args;
 	}
 
 	/**

e89250657174d54d100d161b0978fdf694321cb6
David Shanske <david@shanske.com>
2017-06-17 21:27:01 -0500

    Add u-photo to images with post format image

--- includes/media.php
+++ includes/media.php
@@ -9,9 +9,22 @@ class UF2_Media {
 	 * Initialize plugin
 	 */
 	public function __construct() {
-		// check if theme already supports Microformats2
-		if ( current_theme_supports( 'microformats2' ) ) {
-			return;
+		add_filter( 'wp_get_attachment_image_attributes', array( $this, 'wp_get_attachment_image_attributes' ), 10, 2 );
+	}
+
+	public static function wp_get_attachment_image_attributes( array $attr, WP_Post $attachment ) {
+		$parents = get_post_ancestors( $attachment );
+		$id = $parents[ count( $parents ) -1 ];
+		if ( 'image' !== get_post_format( $id ) ) {
+			return $attr;
 		}
+		if ( isset( $attr['class'] ) ) {
+			$class = explode( ' ', $attr['class'] );
+			$class[] = 'u-photo';
+			$attr['class'] = implode( ' ', array_unique( $class ) );
+		} else {
+			$attr['class'] = 'u-photo';
+		}
+		return $attr;
 	}
 }
